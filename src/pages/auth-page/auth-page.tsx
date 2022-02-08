315f5486907bbfc4cb6a8779de23ef1071abbba3
Mnats <mnatssteam@mail.ru>
2022-02-08 20:54:05 +0300

    feat: auth-page

--- src/pages/auth-page/auth-page.tsx
+++ src/pages/auth-page/auth-page.tsx
@@ -2,6 +2,11 @@ import './auth-page.css';
 
 export const AuthPage = (): JSX.Element => (
   <div className='auth-page'>
-    <h1>login</h1>
+    <form className='box' action='auth-page.tsx' method='post'>
+      <h1>login</h1>
+      <input type='Text' name='' placeholder='Username' />
+      <input type='password' name='' placeholder='Password' />
+      <input type='submit' name='' value='Login' />
+    </form>
   </div>
 );
